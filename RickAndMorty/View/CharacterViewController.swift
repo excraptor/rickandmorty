5d7431239c79666ca08837de419a6ebc1cf1e88a
Tamás Balla <tamas.balla@4ig.hu>
2022-10-13 22:36:24 +0200

    Added sorting possibility and search bar to sort/filter characters

--- RickAndMorty/View/CharacterViewController.swift
+++ RickAndMorty/View/CharacterViewController.swift
@@ -14,8 +14,14 @@ class CharacterViewController: UIViewController {
     public weak var coordinator: MainCoordinator?
     
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchBar: UISearchBar!
     
     private var characterData: Characters? {
+        didSet {
+            characters = characterData?.results
+        }
+    }
+    private var characters: [CharacterModel]? {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -30,6 +36,14 @@ class CharacterViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+        
+        let sortMenu = initSortMenu()
+        let sortButton = UIBarButtonItem(title: "Menu", image: UIImage(systemName: "list.bullet"), primaryAction: nil, menu: sortMenu)
+        sortButton.tintColor = .gray
+        navigationItem.rightBarButtonItem = sortButton
+        
+
         
         tableView.register(UINib(nibName: "CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "characterCell")
         
@@ -50,12 +64,41 @@ class CharacterViewController: UIViewController {
     private func navigateToDetail() {
         // TODO: implement - but rather implement Coordinator pattern
     }
+    
+    private func initSortMenu() -> UIMenu {
+        
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Name ascendig", image: UIImage(systemName: "arrow.down"), handler: { _ in
+                    self.sort(ascending: true)
+                }),
+                UIAction(title: "Name descending", image: UIImage(systemName: "arrow.up"), handler: { _ in
+                    self.sort(ascending: false)
+                }),
+            ]
+        }
+
+        var sortMenu: UIMenu {
+            return UIMenu(title: "Sort", image: nil, identifier: nil, options: [], children: menuItems)
+        }
+        return sortMenu
+    }
+    
+    @objc private func sort(ascending: Bool) {
+        if ascending {
+            characters?.sort() { $0.name < $1.name}
+        } else {
+            characters?.sort() { $0.name > $1.name}
+        }
+        updateUI()
+    }
+    
 }
 
 extension CharacterViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let characterData = characterData else { return 0 }
-        return characterData.results.count
+        guard let characters = characters else { return 0 }
+        return characters.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -64,7 +107,7 @@ extension CharacterViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let characterID = characterData?.results[indexPath.row].id
+        let characterID = characters?[indexPath.row].id
         coordinator?.showDetails(forCharacter: characterID!)
     }
 }
@@ -73,9 +116,16 @@ extension CharacterViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath) as! CharacterTableViewCell
         cell.selectionStyle = .blue
-        guard let characterData = characterData else { return UITableViewCell()}
-        let cellData = characterData.results[indexPath.row]
+        guard let characters = characters else { return UITableViewCell()}
+        let cellData = characters[indexPath.row]
         cell.configure(withData: CharacterListData(name: cellData.name, status: cellData.status, origin: cellData.origin))
         return cell
+    }
+}
+
+extension CharacterViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        characters = searchText.isEmpty ? characterData?.results : characters?.filter { $0.name.contains(searchText) }
+        tableView.reloadData()
     }
 }
